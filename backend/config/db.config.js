3b8a29c1c3170ea11fb1c4e6c6f78293becf43e7
Pedro Rocha <phr123silva@gmail.com>
2024-04-16 12:25:03 -0300

    conexão com o db

---
+++ backend/config/db.config.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const env = {
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  dialect: process.env.DATABASE_DIALECT,
+  jwtsecret: process.env.JWT_SECRET
+}
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(env.database, env.username, env.password, {
+  host: env.host,
+  dialect: env.dialect,
+  operatorAliases: false
+});
+
+const db = [];
+
+db.Cliente = require('../model/Cliente')(sequelize, Sequelize);
+db.Funcionario = require('../model/Funcionario')(sequelize, Sequelize);
+db.Produtos = require('../model/Produtos')(sequelize, Sequelize);
+
+db.sequelize = new Sequelize(env.database, env.username, env.password, {
+  host: env.host,
+  dialect: env.dialect,
+  operatorAliases: false
+});
+db.jwtsecret = env.jwtsecret;
+
+module.exports = db;
+
