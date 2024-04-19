db5803c7ff0881b0d32447e59d026ac574a891bf
Pedro Rocha <phr123silva@gmail.com>
2024-04-19 11:57:18 -0300

    controller funcionario

--- backend/controllers/funcionarioController.js
+++ backend/controllers/funcionarioController.js
@@ -21,3 +21,52 @@ exports.createFuncionario = async (req, res) => {
     })
   }
 }
+
+exports.deleteFuncionario = async (req, res) => {
+  try{
+    const funcionarioId = req.params.id;
+    const codigoExclusao = req.params.codigoExclusao;
+
+    const funcionario = await Funcionario.findByPk(funcionarioId);
+
+    if (!funcionario) {
+      return res.status(404).json({
+        message: "Funcionario não encontrado com o ID fornecido",
+        error: "404"
+      });
+    }
+
+    const codigoEsperado = codigoExclusao;
+
+    if (codigoEsperado != codigoExclusao) {
+      return res.status(401).json({
+        message: "Código de exclusão incorreto. A exclusão requer o código correto",
+        error: "401"
+      });
+    }
+
+    await funcionario.destroy();
+
+    return res.status(200).json({
+      message: "Funcionário deletado com sucesso"
+    });
+  }catch(error){
+    return res.status(500).json({
+      message: "Erro ao deletar funcionário",
+      error: error.message
+    })
+  }
+}
+
+exports.updateFuncionario = async (req, res) => {
+  try{
+    let funcionario = await Funcionario.findByPk(req.body.id);
+
+    if (!funcionario) {
+      return res.status(404).json({
+        message: "Funcionário não encontrado com o ID fornecido",
+        error: "404"
+      });
+    }
+  }
+}
